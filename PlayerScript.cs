b19ac1d24f558688a63f6cf0f368e89d44e43ca0
Chloe <80842883+chloep23@users.noreply.github.com>
2023-09-08 02:11:55 -0400

    Update PlayerScript.cs

--- PlayerScript.cs
+++ PlayerScript.cs
@@ -1,3 +1,8 @@
+// Player Script - 
+// Name: Chloe Park 
+// Aimed Completion Date: October 17, 2023
+// Purpose: Scripts for Player Movement
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +14,7 @@ public class player_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Walking Function 
+        // Walking Function - 
         Vector3 direction = new Vector3(-Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         transform.position += direction * MoveSpeed * Time.deltaTime;
         transform.LookAt(transform.position + direction); //rotating character when walking
